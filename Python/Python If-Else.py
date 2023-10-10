df87ae130c1932de6a9b359f9ee1dbf5953cc61f
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2023-10-10 11:58:14 +0530

    Python If-Else.py updated

--- Python/Python If-Else.py
+++ Python/Python If-Else.py
@@ -9,11 +9,12 @@ import sys
 n=int(input())
 if n%2 != 0:
     print("Weird")
-elif n%2==0 and 2>n>5:
-    print("Not Weird")
-elif n%2==0 and 6>n>20:
-    print("Weird")
-elif n%2==0 and n>20:
-    print("Not Weird")
-else:
-    print("Not Weird")
+elif n%2==0:
+    if 2<=n and n<=5:
+        print("Not Weird")
+    elif 6<=n and n<=20:
+        print("Weird")
+    elif n>=20:
+        print("Not Weird")
+    else:
+        print("Not Weird")
